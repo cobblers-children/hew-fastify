4e5dc8772dc63a4446c309f0434a54d9df6aa6f2
Jason <jdmarshall@users.noreply.github.com>
2022-01-03 20:13:28 -0800

    Test framework.

---
+++ test/test.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const chai = require("chai");
+const sinon = require("sinon");
+const expect = chai.expect;
+
+chai.use(require("sinon-chai"));
+chai.use(require("chai-as-promised"));
+
+describe("suite", function() {
+    beforeEach(function() {
+    });
+});
